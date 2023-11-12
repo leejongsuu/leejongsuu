19fc85a0cd4dc4a932d1f0c555aba8cf54c10dbd
LeeJongsu <lik9819@naver.com>
2023-11-13 00:07:20 +0900

    [Bronze II] Title: 단어의 개수, Time: 464 ms, Memory: 31484 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
-
-public class Main {
-
+import java.util.Arrays;
+class Main {
+	
 	public static void main(String[] args) {
-
-		Scanner sc=new Scanner(System.in);
+	
+		Scanner sc = new Scanner(System.in);
 		
-		String str=sc.nextLine();
-		
-		if(str.length()==1&&str.charAt(0)==' ')
-			{
+		String str= sc.nextLine();
+		int a = 1;
+		if(str.length()==1 && str.charAt(0)==' ')
+		{
 			System.out.println(0);
 			return;
-			}
+		}
 		
-		str=str.trim();
+		str= str.trim();
 		
-		int count=1;
-		
-		for(int i=0;i<str.length();i++)
+		for(int i =0; i<str.length(); i++)
+		{
 			if(str.charAt(i)==' ')
-				count++;
-		System.out.println(count);
-				
+			{
+				a++;
+			}
+		}
+		System.out.println(a);
 	}
 }
